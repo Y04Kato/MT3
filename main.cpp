369903e8408f178720f9f2f24b6f1791791c5944
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-04-21 17:15:59 +0900

    提出

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
 #include<math.h>
 #include<assert.h>
-#define _USE_MATH_DEFINES
+#include "Vector3.h"
 
-typedef struct Matrix4x4 {//4x4の行列を表す
+struct Matrix4x4 {//4x4の行列を表す
 	float m[4][4];
-};
-
-typedef struct Vector3 {//3次元ベクトルを表す
-	float x;
-	float y;
-	float z;
 };
 
 Matrix4x4 MakeTranslateMatrix(Vector3 translate) {
@@ -18,22 +13,55 @@ Matrix4x4 MakeTranslateMatrix(Vector3 translate) {
 	result.m[0][0] = 1.0f;
 	result.m[0][1] = 0.0f;
 	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
+
 	result.m[1][0] = 0.0f;
 	result.m[1][1] = 1.0f;
 	result.m[1][2] = 0.0f;
-	result.m[2][0] = translate.x;
-	result.m[2][1] = translate.y;
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
 	result.m[2][2] = 1.0f;
+	result.m[2][3] = 0.0f;
+
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[3][3] = 1.0f;
+
 	return result;
 };
 
 Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result;
 
+	result.m[0][0] = scale.x;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
+
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = scale.y;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = scale.z;
+	result.m[2][3] = 0.0f;
+
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
+
+	return result;
 }
 
 Vector3 Transform(Vector3 vector, Matrix4x4 matrix) {
 	Vector3 result;
-	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[1][0] +  1.0f * matrix.m[3][0];
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[1][0] + 1.0f * matrix.m[3][0];
 	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
 	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
 	float w = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
@@ -43,6 +71,25 @@ Vector3 Transform(Vector3 vector, Matrix4x4 matrix) {
 	result.z /= w;
 	return result;
 };
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector3, const char* name) {
+	Novice::ScreenPrintf(x + 1 * kColumnWidth, y + kRowHeight, "%6.02f", vector3.x);
+	Novice::ScreenPrintf(x + 2 * kColumnWidth, y + kRowHeight, "%6.02f", vector3.y);
+	Novice::ScreenPrintf(x + 3 * kColumnWidth, y + kRowHeight, "%6.02f", vector3.z);
+	Novice::ScreenPrintf(x + 4 * kColumnWidth, y + kRowHeight, "%s", name);
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name) {
+	Novice::ScreenPrintf(x, y, "%s", name);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight + 20, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
 
 const char kWindowTitle[] = "LE2B_07_カトウユウタ_タイトル";
 
@@ -60,7 +107,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 translate{ 4.1f,2.6f,0.8f };
 	Vector3 scale{ 1.5f,5.2f,7.3f };
 	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-	Matrix4x4 scalaMatrix = MakeScaleMatrix(scale);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
 	Vector3 point{ 2.3f,3.8f,1.4f };
 	Matrix4x4 transformMatrix = {
 		1.0f,2.0f,3.0f,4.0f,
@@ -93,7 +140,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, 0, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, scaleMatrix, "scaleMatrix");
 
 		///
 		/// ↑描画処理ここまで
@@ -112,4 +161,3 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Finalize();
 	return 0;
 }
-
