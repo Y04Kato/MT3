c78c5e7d19ecae8da4f553174b67fa8fb0a952f2
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-04-21 13:02:43 +0900

    00-03

--- main.cpp
+++ main.cpp
@@ -1,4 +1,48 @@
 #include <Novice.h>
+#include<math.h>
+#include<assert.h>
+#define _USE_MATH_DEFINES
+
+typedef struct Matrix4x4 {//4x4の行列を表す
+	float m[4][4];
+};
+
+typedef struct Vector3 {//3次元ベクトルを表す
+	float x;
+	float y;
+	float z;
+};
+
+Matrix4x4 MakeTranslateMatrix(Vector3 translate) {
+	Matrix4x4 result;
+	result.m[0][0] = 1.0f;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = 1.0f;
+	result.m[1][2] = 0.0f;
+	result.m[2][0] = translate.x;
+	result.m[2][1] = translate.y;
+	result.m[2][2] = 1.0f;
+	return result;
+};
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+
+}
+
+Vector3 Transform(Vector3 vector, Matrix4x4 matrix) {
+	Vector3 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[1][0] +  1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
+};
 
 const char kWindowTitle[] = "LE2B_07_カトウユウタ_タイトル";
 
@@ -11,6 +55,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	//宣言
+	Vector3 translate{ 4.1f,2.6f,0.8f };
+	Vector3 scale{ 1.5f,5.2f,7.3f };
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	Matrix4x4 scalaMatrix = MakeScaleMatrix(scale);
+	Vector3 point{ 2.3f,3.8f,1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f,
+	};
+	Vector3 transformed = Transform(point, transformMatrix);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +83,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +92,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+
 
 		///
 		/// ↑描画処理ここまで
