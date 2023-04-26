6278b8812913d636d4ee8e672f27cbbdaea0635e
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-04-26 15:49:35 +0900

    VectorScreenPrintfの追加

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <Novice.h>
+#include "Vector3.h"
+#include "Matrix4x4.h"
 
 const char kWindowTitle[] = "LE2B_07_カトウユウタ_タイトル";
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector3, const char* name) {
+	Novice::ScreenPrintf(x + 1 * kColumnWidth, y + kRowHeight, "%6.02f", vector3.x);
+	Novice::ScreenPrintf(x + 2 * kColumnWidth, y + kRowHeight, "%6.02f", vector3.y);
+	Novice::ScreenPrintf(x + 3 * kColumnWidth, y + kRowHeight, "%6.02f", vector3.z);
+	Novice::ScreenPrintf(x + 4 * kColumnWidth, y + kRowHeight, "%s", name);
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name) {
+	Novice::ScreenPrintf(x, y, "%s", name);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight + 20, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
