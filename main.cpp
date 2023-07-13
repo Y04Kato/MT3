68f60552cdea9f17a234fdf396dcb31f44414679
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-07-13 15:03:39 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -80,8 +80,7 @@ Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 	return result;
 }
 
-Matrix4x4 Inverse(const Matrix4x4& m)
-{
+Matrix4x4 Inverse(const Matrix4x4& m){
 	Matrix4x4 result;
 	float determinant =
 		m.m[0][0] * m.m[1][1] * m.m[2][2] * m.m[3][3] + m.m[0][0] * m.m[1][2] * m.m[2][3] * m.m[3][1] + m.m[0][0] * m.m[1][3] * m.m[2][1] * m.m[3][2]
